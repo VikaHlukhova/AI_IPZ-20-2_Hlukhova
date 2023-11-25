08e92202ec9ecb20ec40e07ea07ed066310ce0a1
Vika Hlukhova <ipz202_gvs@student.ztu.edu.ua>
2023-11-25 20:21:54 +0200

    Lab_6

---
+++ Lab_6/LR_6_task_5.py
@@ -0,0 +1,61 @@
+import numpy as np
+import neurolab as nl
+
+# Г В С
+target = [[1, 1, 0, 1, 1,
+           1, 0, 0, 0, 0,
+           0, 0, 0, 0, 1,
+           1, 0, 1, 0, 1,
+           0, 0, 1, 0, 1],
+          [0, 1, 0, 1, 0,
+           1, 0, 1, 0, 1,
+           1, 1, 1, 1, 1,
+           0, 0, 0, 0, 0,
+           1, 1, 0, 1, 1],
+          [0, 0, 1, 1, 1,
+           1, 0, 0, 0, 1,
+           1, 0, 0, 1, 0,
+           1, 1, 0, 1, 0,
+           0, 0, 0, 1, 1]]
+
+chars = ['Г', 'В', 'С']
+target = np.asfarray(target)
+target[target == 0] = -1
+
+# Create and train network
+net = nl.net.newhop(target)
+
+output = net.sim(target)
+print("Test on train samples:")
+for i in range(len(target)):
+    print(chars[i], (output[i] == target[i]).all())
+
+print("\nTest on defaced Г:")
+test = np.asfarray([1, 1, 0, 1, 1,
+                    1, 0, 0, 0, 1,
+                    0, 0, 1, 0, 1,
+                    1, 0, 1, 0, 1,
+                    0, 0, 1, 0, 1])
+test[test == 0] = -1
+out = net.sim([test])
+print((out[0] == target[0]).all(), 'Sim. steps', len(net.layers[0].outs))
+
+print("Test of defaced В:")
+test = np.asfarray([0, 1, 0, 1, 0,
+                    1, 0, 1, 0, 1,
+                    1, 1, 0, 1, 1,
+                    0, 0, 0, 0, 1,
+                    1, 1, 0, 1, 1])
+test[test == 0] = -1
+output = net.sim([test])
+print((output[0] == target[1]).all(), 'Sim. steps', len(net.layers[0].outs))
+
+print("Test of defaced С:")
+test = np.asfarray([0, 0, 1, 1, 1,
+                    1, 1, 0, 0, 1,
+                    1, 0, 0, 1, 0,
+                    1, 1, 0, 0, 0,
+                    0, 0, 0, 1, 1])
+test[test == 0] = -1
+output = net.sim([test])
+print((output[0] == target[2]).all(), 'Sim. steps', len(net.layers[0].outs))
